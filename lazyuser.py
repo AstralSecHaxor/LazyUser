306b26865d0e19e8f3774be18df80c799d1e9002
AstralSec Haxor <91766268+AstralSecHaxor@users.noreply.github.com>
2023-10-01 03:02:35 -0300

    update lazyuser.py

--- lazyuser.py
+++ lazyuser.py
@@ -28,7 +28,7 @@ am = "\033[33m" #Amarelo
 
 def check_proot():
     
-    if isdir("/etc/apt") == False:
+    if isdir("/etc/apt") == True:
         print(f"{bra}[{vrd}*{bra}] {bra}Termux-chroot está ativo.");timeout(1)
         banner_inicial()
         while True:
