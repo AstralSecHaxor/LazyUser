d5618be13cae673545a417050fc595d7da4a3daa
AstralSec Haxor <91766268+AstralSecHaxor@users.noreply.github.com>
2023-10-11 19:15:32 -0300

    update install.py

--- install.py
+++ install.py
@@ -21,7 +21,6 @@ def executavel():
     os.system(f"rm -rf {diretorio}/usertool")
     os.system(f"cp -r {diretorio}/LazyUser/modules/usertool {diretorio}")
     os.system(f"chmod +x {diretorio}/usertool") 
-    os.system(f" cp -r .git/ {diretorio}")
     print(f"""
     {vlh}Uso:{bra}
   Para invocar a ferramenta, utilize um dos seguintes comandos:
@@ -68,10 +67,10 @@ def executavel_termux():
     os.system(f"rm -rf {diretorio_termux}/UserTool")
     os.system(f"cp -r {diretorio_termux}/LazyUser/modules/UserTool {diretorio_termux}")
     os.system(f"chmod +x {diretorio_termux}/UserTool") 
+    
     os.system(f"rm -rf {diretorio_termux}/usertool")
     os.system(f"cp -r {diretorio_termux}/LazyUser/modules/usertool {diretorio_termux}")
     os.system(f"chmod +x {diretorio_termux}/usertool") 
-    os.system(f" cp -r .git/ {diretorio_termux}")
     print(f"""
 {vlh}Uso:{bra}
       Para invocar a ferramenta, utilize um dos seguintes comandos:
