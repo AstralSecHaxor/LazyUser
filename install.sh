757853a37621185c79adf1c470812571fb789930
AstralSec Haxor <91766268+AstralSecHaxor@users.noreply.github.com>
2023-12-03 21:27:35 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -35,18 +35,18 @@ echo -e "         ${azul}+${magenta}-- -${azul}+${amarelo}=${branco}[ By: Astral
 # Função para verificar se um comando foi executado com sucesso
 function verificar_sucesso {
   if [ $? -eq 0 ]; then
-    echo -e "\n[${verde}✔${reset}] ${ciano}instalação concluída com sucesso.${reset}"
+    echo -e "\n[${verde} ✔ ${reset}] ${ciano}instalação concluída com sucesso.${reset}"
   else
-    echo -e "\n[${vermelho}!${reset}]${vermelho} A instalação do Tool falhou devido a um erro durante o processo.${reset}"
-    echo -e "\n[${vermelho}!${reset}]${vermelho} Lamento, mas não foi possível continuar :(${reset}"
+    echo -e "\n[${vermelho} !! ${reset}]${vermelho} A instalação do Tool falhou devido a um erro durante o processo.${reset}"
+    echo -e "\n[${vermelho} !! ${reset}]${vermelho} Lamento, mas não foi possível continuar :(${reset}"
     exit 1
   fi
 }
 
 # Atualiza a lista de pacotes e faz o upgrade
-echo -e "\n\n${reset}[${amarelo}-${reset}] ${verde}Atualizando lista de pacotes e fazendo upgrade...${reset}"
+echo -e "\n\n${reset}[${magenta} ... ${reset}] ${verde}Atualizando lista de pacotes e fazendo upgrade...${reset}"
 apt-get update -y && apt-get upgrade -y
-echo -e "\n${reset}[${verde}✔${reset}] ${ciano}Atualizando concluída${reset}"
+echo -e "\n${reset}[${verde} ✔ ${reset}] ${ciano}Atualizando concluída. ${reset}"
 # Verificando se a atualização foi bem-sucedida
 
 # Lista de pacotes
@@ -55,10 +55,10 @@ dependencies=("git" "python3" "pip")
 for package in "${dependencies[@]}"; do
     ## Checando se o pacote já foi instalado
     if dpkg -l | grep -q "^ii.*$package "; then
-        echo -e "\n[${verde}✔${reset}]${amarelo} $package ${verde}está instalado.${reset}"
+        echo -e "\n[${verde} ✔ ${reset}]${amarelo} $package ${verde}está instalado.${reset}"
     else
         ## Fazendo a instalação do pacote
-        echo -e "\n[${amarelo}-${reset}] ${verde}Instalando -->${amarelo} $package... ${reset}"
+        echo -e "\n[${magenta} ... ${reset}] ${verde}Instalando -->${amarelo} $package ... ${reset}"
         # Comando de instalação dos pacotes
         apt-get install -y "$package"
         # Verifique se a instalação foi bem-sucedida
@@ -66,15 +66,13 @@ for package in "${dependencies[@]}"; do
     fi
 done
 
-# Instalação dos pacotes da lib Pillow
-echo -e "\n[${amarelo}-${reset}] ${verde}Instalando pacotes necessários...${reset}"
-apt install python make wget termux-exec clang libjpeg-turbo freetype -y
-echo -e "\n[${verde}✔${reset}] ${ciano}Pacotes instalados com sucesso.${reset}"
-
 # Instalação da biblioteca Pillow
-echo -e "\n[${amarelo}-${reset}] ${verde}Instalando a biblioteca Pillow...${reset}"
+echo -e "\n[${magenta} ... ${reset}] ${verde}Instalando  biblioteca --> ${amarelo}Pillow...${reset}"
+apt install python make wget termux-exec clang libjpeg-turbo freetype -y
 env INCLUDE="$PREFIX/include" LDFLAGS=" -lm" pip install Pillow
+
 verificar_sucesso
+
 
 
 # instalação de libs
@@ -82,10 +80,10 @@ dependencies=("python-telegram-bot" "requests")
 for package in "${dependencies[@]}"; do
     ## Checando se o pacote já foi instalado
     if command -v "$package" >/dev/null 2>&1; then
-        echo -e "\n[${verde}✔${reset}]${amarelo} $package ${verde}está instalado.${reset}"
+        echo -e "\n[${verde} ✔ ${reset}]${amarelo} $package ${verde}está instalado.${reset}"
     else
         ## Fazendo a instalação do pacote usando pip
-        echo -e "\n[${amarelo}-${reset}] ${verde}Instalando -->${amarelo} $package... ${reset}"
+        echo -e "\n[${magenta} ... ${reset}] ${verde}Instalando biblioteca -->${amarelo} $package ... ${reset}"
         # Comando de instalação dos pacotes usando pip
         pip install "$package"
         # Verifique se a instalação foi bem-sucedida
@@ -93,12 +91,12 @@ for package in "${dependencies[@]}"; do
     fi
 done
 # Clona o repositório LazyUser e executa install.py
-echo -e "\n[${amarelo}-${reset}]${ciano} Clonando o repositório ${amarelo}'LazyUser'${verde} ${ciano}e executando ${amarelo}'setup.py'${verde}...${reset}"
+echo -e "\n[${magenta} ... ${reset}]${verde} Clonando o repositório ${amarelo}'LazyUser'${verde} ${verde}e executando ${amarelo}'setup.py'${verde} ...${reset}"
 git clone https://github.com/AstralSecHaxor/LazyUser
 verificar_sucesso
+echo -e " "
 python3 LazyUser/setup.py
-#rm -rf LazyUser
-echo -e "\n[${verde}✔${reset}]${ciano}iTudo foi concluído com sucesso!${reset}"
+rm -rf LazyUser
 exit
 
 ###wget -O script.sh https://raw.githubusercontent.com/usuario/repositorio/master/script.sh && chmod +x script.sh && ./script.sh
